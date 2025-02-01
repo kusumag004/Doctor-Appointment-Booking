3c52033983503724d18daf1fc5513260d63f4680
Muskaan <muskan@example.com>
2025-02-01 14:59:49 +0530

    Initial frontend setup including navbar, header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Doctors from './pages/Doctors';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Myprofile from './pages/MyProfile';
+import Login  from './pages/Login'; // Named import
+import Appointment from './pages/Appointment';
+import Contact from './pages/Contact';
+
+import MyAppointments from './pages/MyAppointments'
+
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+    <Navbar />
+     <Routes>
+     <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path ='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element = {<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<Myprofile />} />
+        <Route path ='/my-appointments' element = {<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+
+     </Routes>
+    </div>
+  )
+}
+
+export default App
+
