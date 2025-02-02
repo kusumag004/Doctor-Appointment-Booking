b6a68c55e9ec169e540fbc59671f427c801b2db1
Mansi8588 <younliacmh@gmail.com>
2025-02-02 15:17:46 +0530

    models

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
-// import connectCloudinary from './config/cloudinary.js'
+import connectCloudinary from './config/cloudinary.js'
 // import adminRouter from './routes/adminRoute.js'
 
 
@@ -11,7 +11,7 @@ import connectDB from './config/mongodb.js'
 const app=express()
 const port =process.env.PORT|| 4000
 connectDB()
-// connectCloudinary()
+connectCloudinary()
 
 // // middlewares
 app.use(express.json())  // any request parse 
