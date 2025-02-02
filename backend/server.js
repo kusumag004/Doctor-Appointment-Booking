a4aa2cde4093a3c05c98815b0070a3c61de8fc6b
Mansi8588 <younliacmh@gmail.com>
2025-02-02 15:30:53 +0530

    admin addDoctor api

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
-// import adminRouter from './routes/adminRoute.js'
+import adminRouter from './routes/adminRoute.js'
 
 
 
@@ -18,7 +18,7 @@ app.use(express.json())  // any request parse
 app.use(cors())   
 
 // // api endpoints
-// app.use('/api/admin',adminRouter)
+app.use('/api/admin',adminRouter)
 
 // localhost:4000/api/admin/add-doctor
 
