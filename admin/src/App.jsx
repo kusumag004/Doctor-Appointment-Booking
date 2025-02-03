d74b7e3a7bbed0b42cf64149abc4dd3dd889bff8
hema1mathi <hemasantoshimathi@gmail.com>
2025-02-03 16:04:49 +0530

    Add doctor functionality in Admin Panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllApointments from './pages/Admin/AllApointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
   const {aToken}=useContext(AdminContext)
@@ -13,6 +19,16 @@ const App = () => {
       
       <ToastContainer/>
       <Navbar/>
+      <div className = 'flex items-start'>
+        <Sidebar />
+        <Routes >
+          <Route path = '/' element = {<></>} />
+          <Route path = '/admin-dashboard' element = {<Dashboard/>} />
+          <Route path = '/all-appointments' element = {<AllApointments/>} />
+          <Route path = '/add-doctor' element = {<AddDoctor/>} />
+          <Route path = '/doctor-list' element = {<DoctorsList/>} />
+        </Routes>
+      </div>
     </div>
   ):(
     <>
