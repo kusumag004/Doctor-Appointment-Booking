ba596fd5ac2a7cb9dc3ccb460b5384e4f10bc764
hema1mathi <hemasantoshimathi@gmail.com>
2025-02-03 17:16:34 +0530

    doctor info added to datadase

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import {assets} from '../../assets/assets'
 import { useState } from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const AddDoctor = () => {
 
@@ -15,8 +19,59 @@ const AddDoctor = () => {
     const [degree,setDegree] = useState('')
     const [address1,setAddress1] = useState('')
     const [address2,setAddress2] = useState('')
+
+    const {backendUrl, aToken} = useContext(AdminContext)
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault()
+
+        try {
+            if(!docImg){
+                return toast.error('Image Not Selected')
+            }
+            const formData = new FormData()
+
+            formData.append('image',docImg)
+            formData.append('name',name)
+            formData.append('email',email)
+            formData.append('password',password)
+            formData.append('experience',experience)
+            formData.append('fees',Number(fees))
+            formData.append('about',about)
+            formData.append('speciality',speciality)
+            formData.append('degree',degree)
+            formData.append('address',JSON.stringify({line1:address1,line2:address2}))
+
+            //console log formdata
+            formData.forEach((value,key)=>{
+                console.log(`${key} : ${value}`);
+            })
+
+            const {data} = await axios.post(backendUrl + '/api/admin/add-doctor',formData,{headers:{aToken}})
+
+            if(data.success){
+                toast.success(data.message)
+                setDocImg(false)
+                setName('')
+                setPassword('')
+                setEmail('')
+                setAddress1('')
+                setAddress2('')
+                setDegree('')
+                setAbout('')
+                setFees('')
+            }else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
+    }
+
   return (
-    <form className='m-5 w-full'>
+    <form onSubmit={onSubmitHandler} className='m-5 w-full'>
         <p className='mb-3 text-1g font-medium'>Add Doctor</p>
         <div className='bg-white px-8 py-8 border rounded w-full max-w-4x1 max-h-[80vh] overflow-y-scroll'>
             <div className='flex items-center gap-4 mb-8 text-gray-500'>
