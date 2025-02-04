859ac887017ac7891d56c41b4a5f33cdf25ed2ee
Muskaan <muskanyadavonline@gmail.com>
2025-02-04 19:48:55 +0530

    Added user login API (POST /api/login/user) with JWT authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,5 +63,57 @@ res.json({success:true,token})
 
 }
 
+// API for user login
 
-export {registerUser}
+
+const loginUser = async (req,res) => {
+
+
+try{
+
+const {email,password} = req.body
+const user= await userModel.findOne ({email})
+
+
+if(!user){
+    return res.json({success:false, message:'user does not exist'})
+
+}
+
+const isMatch = await bcrypt.compare(password,user.password)
+
+if(isMatch){
+
+const token= jwt.sign({id:user._id}, process.env.JWT_SECRET)
+res.json({success:true, token})
+
+}
+else{
+    res.json({success:false,message:"Invalid credentials"})
+}
+
+
+
+
+} catch (error){
+
+console.log(error)
+res.json({success:false, message:error.message})
+
+
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+export {registerUser,loginUser}
