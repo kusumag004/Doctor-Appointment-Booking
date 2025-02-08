96e7fc034ea4a386f1c5c4cb47a91e1194ef7e89
kusumag004 <kusumag004@gmail.com>
2025-02-08 11:50:25 +0530

    Display appointments in frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -271,22 +271,26 @@ res.json({success:true,message:'Appointment booked'})
 console.log(error)
 res.json({success:false, message:error.message})
 
-
-
-
+}
 
 }
 
+// API to get user appointments for frontend my-appointments page
+const listAppointment = async (req,res) => {
 
+    try{
 
+        const {userId} = req.body
+        const appointments = await appointmentModel.find({userId})
+
+        res.json({success:true,appointments})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+
+    }
 }
 
 
-
-
-
-
-
-
-
-export {registerUser,loginUser, getProfile, updateProfile, bookAppointment}
+export {registerUser,loginUser, getProfile, updateProfile, bookAppointment, listAppointment}
