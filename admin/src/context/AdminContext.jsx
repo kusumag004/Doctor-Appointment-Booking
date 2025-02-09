83e66febe856de69967018847c01b3b4c34aa749
hema1mathi <hemasantoshimathi@gmail.com>
2025-02-09 21:53:55 +0530

    Dashboard Page for admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
 
   const [doctors,setDoctors] = useState([])
   const [appointments,setAppointments] = useState([])
+  const [dashData, setDashData] = useState(false)
 
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
@@ -81,6 +82,23 @@ const AdminContextProvider = (props) => {
     }
   }
 
+  const getDashData = async() => {
+    try {
+
+      const {data} = await axios.get(backendUrl + '/api/admin/dashboard',{headers:{aToken}})
+
+      if(data.success){
+        setDashData(data.dashData)
+        console.log(data.dashData)
+      }else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   const value = {
     aToken,
     setAToken,
@@ -91,7 +109,8 @@ const AdminContextProvider = (props) => {
     appointments,
     setAppointments,
     getAllAppointments,
-    cancelAppointment
+    cancelAppointment,
+    dashData,getDashData
   }
 
 
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { addDoctor ,allDoctors,loginAdmin,appointmentsAdmin, appointmentCancel} from '../controllers/adminController.js'
+import { addDoctor ,allDoctors,loginAdmin,appointmentsAdmin, appointmentCancel, adminDashboard} from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
 import { changeAvailability } from '../controllers/doctorControllers.js'
@@ -13,6 +13,7 @@ adminRouter.post('/all-doctors',authAdmin,allDoctors)
 adminRouter.post('/change-availability',authAdmin,changeAvailability)
 adminRouter.get('/appointments',appointmentsAdmin)
 adminRouter.post('/cancel-appointment',authAdmin,appointmentCancel)
+adminRouter.get('/dashboard',authAdmin,adminDashboard)
 
 
 export default adminRouter
